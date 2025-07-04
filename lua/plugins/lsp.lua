e1f28989279b8f22b66e4f4aa3b0118c665dc5ed
ngferg <me@nicholaus-ferguson.us>
2025-07-03 23:51:30 -0500

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,7 +14,6 @@ return {
     },
     config = function()
       require("lspconfig").lua_ls.setup {}
-
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "rust", "java", "javascript", "css", "html" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "java", "javascript", "css", "html" },
         auto_install = true,
         highlight = {
           enable = true,
