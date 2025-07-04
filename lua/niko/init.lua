0f968a87a3f7e482c12cf5d621aa247dcc62dc04
ngferg <me@nicholaus-ferguson.us>
2025-07-03 22:23:19 -0500

    treesitter

--- lua/niko/init.lua
+++ lua/niko/init.lua
@@ -1,6 +1,7 @@
 require("niko.remap")
 
-
+vim.opt.clipboard = "unnamedplus"
 vim.opt.shiftwidth = 4
 vim.opt.title = true
 vim.opt.titlestring  = "%t - nvim"
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "rust", "java", "javascript", "css", "html" },
+	auto_install = true,
+	highlight = {
+	  enable = true,
+	}
+      }
+    end,
+  }
+}
