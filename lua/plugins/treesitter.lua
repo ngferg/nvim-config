c39d5f00d20036284a9937418af232d8cc9213d5
ngferg <me@nicholaus-ferguson.us>
2025-07-03 23:11:42 -0500

    autoformatter in lsp.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,12 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "rust", "java", "javascript", "css", "html" },
-	auto_install = true,
-	highlight = {
-	  enable = true,
-	}
+      require 'nvim-treesitter.configs'.setup {
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "rust", "java", "javascript", "css", "html" },
+        auto_install = true,
+        highlight = {
+          enable = true,
+        }
       }
     end,
   }
