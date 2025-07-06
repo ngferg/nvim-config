2a4d5a13cc3fa86a52c0b3f9a45a6ff99fca11c9
ngferg <me@nicholaus-ferguson.us>
2025-07-05 23:52:39 -0500

    starting telescope

--- lua/niko/remap.lua
+++ lua/niko/remap.lua
@@ -4,5 +4,8 @@ vim.keymap.set("n", "<leader>so", ":luafile %<CR>")
 vim.keymap.set("n", "<leader>ww", ":w<CR>")
 vim.keymap.set("n", "<leader>qq", ":q!<CR>")
 
-vim.keymap.set("n", "<leader>rtest",
+-- telescope
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>");
+
+vim.keymap.set("n", "<leader>rusttest",
   "Go#[cfg(test)]<CR>mod tests {<CR>use super::*;<CR><CR>#[test]<CR>fn test_add(){<CR>assert_eq!(2 + 2, 4);<CR>}<CR>}<CR><Esc>")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,14 @@
 --plugins/telescope.lua:
-return {
+return { {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   -- or                              , branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' }
-}
+  dependencies = { 'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>ff", require('telescope.builtin').find_files)
+    vim.keymap.set("n", "<leader>fr", require('telescope.builtin').lsp_references)
+    vim.keymap.set("n", "<leader>fh", require('telescope.builtin').help_tags)
+  end
+} }
